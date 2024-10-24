7d34252b518a270d01d810c21d4ffb8b529c2d8c
David Krall <davkra@chello.at>
2024-10-24 23:31:14 +0200

    using libTest.so instead of sourcecode for libTester

--- JavaTester/01_build.sh
+++ JavaTester/01_build.sh
@@ -6,4 +6,4 @@ javac -h . MyNativeClass.java
 
 # Build shared object file
 mkdir -p ./lib/
-g++ -I../inc -I"/usr/lib/jvm/java-17-openjdk-amd64/include/" -I"/usr/lib/jvm/java-17-openjdk-amd64/include/linux/" -shared -fPIC -o ./lib/libmyNativeLibrary.so MyNativeClass.cpp ../src/hello.cpp ../src/add.cpp
+g++ -I../inc -I"/usr/lib/jvm/java-17-openjdk-amd64/include/" -I"/usr/lib/jvm/java-17-openjdk-amd64/include/linux/" -shared -fPIC -o ./lib/libmyNativeLibrary.so MyNativeClass.cpp ../lib/libTest.so
