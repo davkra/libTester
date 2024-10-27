95af3c79a03e3b68dcb0e10b24b2fd5de93a8c3b
David Krall <davkra@chello.at>
2024-10-27 11:13:27 +0100

    using jar file for run command

--- javatester/src/main/java/02_run.sh
+++ javatester/src/main/java/02_run.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 export LD_LIBRARY_PATH=../../../../javatester/src/main/java/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=../../../../lib:$LD_LIBRARY_PATH
 
-java -Djava.library.path=../../../../lib:../../../../javatester/src/main/java/lib at.libtester.MyNativeClass
+java -cp ../../../target/javatester-1.0-SNAPSHOT.jar at.libtester.MyNativeClass
